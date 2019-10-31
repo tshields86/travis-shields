e4d367659c5e295d5e52aade80cd2aa386f75fce
Travis Shields <travis.shields@gmail.com>
2019-10-30 21:05:26 -0400

    Add wrapper and skills section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,16 +3,19 @@ import Copyright from '../Copyright/Copyright';
 import Contact from '../Contact/Contact';
 import Header from '../Header/Header';
 import Social from '../Social/Social';
+import Wrapper from '../Wrapper/Wrapper';
 import styles from './Footer.css';
 
 const Footer = () => (
   <footer className={styles.root}>
-    <Header text="Let's Get In Touch!" />
-    <p>Look over my resume, give me a call, or send an email.</p>
-    <Contact />
-    <p>Or check me out here:</p>
-    <Social />
-    <Copyright />
+    <Wrapper>
+      <Header text="Let's Get In Touch!" />
+      <p>Look over my resume, give me a call, or send an email.</p>
+      <Contact />
+      <p>Or check me out here:</p>
+      <Social />
+      <Copyright />
+    </Wrapper>
   </footer>
 );
 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import Skills from '../Skills/Skills';
 import Footer from '../Footer/Footer';
 import styles from './Main.css';
 
 const Main = () => (
   <main className={styles.main}>
-    <div className={styles.main__wrapper}>
-      <Footer />
-    </div>
+    <Skills />
+    <Footer />
   </main>
 );
 
