46c1300fad554ef08550718e051cda41ab4d9df3
Travis Shields <travis.shields@gmail.com>
2020-02-23 17:07:44 -0500

    Adjust grid and title font family

--- src/components/elements/Glitch/Glitch.js
+++ src/components/elements/Glitch/Glitch.js
@@ -6,7 +6,7 @@ import styles from './Glitch.css';
 const Glitch = ({ hasEntered }) => (
   <div className={cx(styles.glitch, { [styles.glitch__off]: hasEntered })}>
     <div className={styles.glitch__area}>
-      <h1 className={styles.glitch__title}>Travis<span>Shields</span></h1>
+      <h1 className={styles.glitch__title}>TRAVIS<span>SHIELDS</span></h1>
       <h2 className={styles.glitch__text}>
         <span>><span className={styles.glitch__blink}>_</span></span>
         fullstack engineer
